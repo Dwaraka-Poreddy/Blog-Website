da37ce1dc775c9cf18673038ec56ff8251545967
unknown <f20170855@hyderabad.bits-pilani.ac.in>
2021-05-04 19:33:09 +0530

    mongodb database added

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
-const posts = [];
+const mongoose = require("mongoose");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -20,44 +19,67 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect(
+  "mongodb+srv://dwaraka:asusasusasus@giftshubblog.r3ihu.mongodb.net/blogDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
   });
 });
-app.get("/about", function (req, res) {
-  res.render("about", {
-    aboutContent: aboutContent,
-  });
-});
-app.get("/contact", function (req, res) {
-  res.render("contact", {
-    contactContent: contactContent,
-  });
-});
+
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = req.params.postName;
-  posts.forEach(function (post) {
-    if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
-      res.render("post", {
-        post: post,
-      });
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
     }
   });
 });
 
-app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.posttitle,
-    postbody: req.body.postbody,
-  };
-  posts.push(post);
-  res.redirect("/");
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
 });
-app.listen(process.env.PORT || 3000, function () {
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
